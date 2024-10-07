1fa370cec2ab06560f68cc88629f4cebb48e3077
Wellington Chida <wellingtonchida@protonmail.com>
2024-10-06 21:14:44 -0300

    feat: change url

--- src/login/infrastructure/database.py
+++ src/login/infrastructure/database.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://myuser:mypassword@localhost:5432/chatbot_db")
 engine = create_engine(DATABASE_URL)
 Session_db = sessionmaker(bind=engine, expire_on_commit=False)
 
