f1f5ab9977efe59ab1eac61c400ddbc50dde8368
Wellington Chida <wellingtonchida@protonmail.com>
2024-10-07 22:57:16 -0300

    feat: add secret to yml and assert db

--- src/login/infrastructure/database.py
+++ src/login/infrastructure/database.py
@@ -7,6 +7,7 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+assert DATABASE_URL is not None, "DATABASE_URL environment variable is not set"
 
 engine = create_engine(DATABASE_URL)
 Session_db = sessionmaker(bind=engine, expire_on_commit=False)
