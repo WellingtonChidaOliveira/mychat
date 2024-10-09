f11cf09c507b1ef558ecc98e37e098548c163ef6
Wellington Chida <wellingtonchida@protonmail.com>
2024-10-09 20:06:23 -0300

    .

--- src/login/domain/user.py
+++ src/login/domain/user.py
@@ -1,14 +1,9 @@
 from sqlalchemy import Column, String
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.types import TypeDecorator, CHAR
-import uuid
 from ..infrastructure.database import Base
-
 
 
 class User(Base):
     __tablename__ = "users"
-
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True,primary_key=True ,index=True)
     hashed_password = Column(String)
--- test/test_database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-# Use SQLite in-memory database for testing
-TEST_DATABASE_URL = "sqlite:///:memory:"
-
-engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
