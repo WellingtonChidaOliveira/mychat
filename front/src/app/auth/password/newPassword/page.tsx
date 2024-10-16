c8990558df57dad8ecea19ca2b9bfffcc8041c05
Kauã Gomes <70298794+Kauagpaiva@users.noreply.github.com>
2024-10-16 19:43:13 -0300

    validação nova senha

--- front/src/app/auth/password/newPassword/page.tsx
+++ front/src/app/auth/password/newPassword/page.tsx
@@ -1,26 +1,51 @@
+"use client";
+
 import Button from "@/components/button";
 import InputField from "@/components/inputField";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { useRouter } from "next/navigation";
 
 export default function SignIn(){
+    const router = useRouter();
+
+    const validationSchema = Yup.object({
+        password: Yup.string().min(6, 'A senha deve ter pelo menos 6 caracteres').required('Senha é obrigatória'),
+        confirmPassword: Yup.string().min(6, 'A senha deve ter pelo menos 6 caracteres').required('Senha é obrigatória'),
+    });
+
+    const sendForm = (values: { password: string; confirmPassword: string }) => {
+        console.log('Dados enviados:', values);
+        router.push('./');
+    };
     return(
         <div className="flex flex-col items-center gap-[42px]">
-
             <span className="text-[30px] font-semibold leading-[37.5px] font-poppins text-[#3ea59f]">Nova senha</span>
-
-            <div className=" w-full flex flex-col gap-[16px]">
-                <p className="text-[18px] font-normal leading-[27px] font-poppins text-[#333333]">
-                    Insira uma nova senha para a sua conta.
-                </p>  
-                <InputField placeholder="Crie uma senha forte" tittle="Nova senha"/>
-                <InputField placeholder="Confirmar senha" tittle="Confirmar senha"/>
-            </div>
-
-            <div className="flex flex-col w-[80%] gap-[16px]">
-                <Button href="/auth" text="Continuar" variant="primary"/>
-            </div>
+            <Formik 
+                initialValues={{ password: '', confirmPassword: '' }}
+                validationSchema={validationSchema}
+                onSubmit={(values) => sendForm(values)}>
+                {({ handleSubmit }) => (
+                    <Form className=" w-full flex flex-col items-center gap-[42px]">
+                        <div className=" w-full flex flex-col gap-[16px]">
+                            <p className="text-[18px] font-normal leading-[27px] font-poppins text-[#333333]">
+                                Insira uma nova senha para a sua conta.
+                            </p> 
+                            <div>
+                                <Field name="password" placeholder="Crie uma senha forte" tittle="Nova senha" component={InputField} />
+                                <ErrorMessage name="password" component="div" className="text-red-500" />
+                            </div>
+                            <div>
+                                <Field name="confirmPassword" placeholder="Confirmar senha" tittle="Confirmar senha" component={InputField} />
+                                <ErrorMessage name="confirmPassword" component="div" className="text-red-500" />
+                            </div>
+                        </div>
+                        <div className="w-[80%]">
+                            <Button text="Continuar" variant="primary" type="submit"/>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
         </div>
-        
-
-        
     )
 }
