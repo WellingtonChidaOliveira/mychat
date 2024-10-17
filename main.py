afd63d461798e7d52b7210cdd0490f755b2b3a83
Wellington Chida <wellingtonchida@protonmail.com>
2024-10-17 00:51:57 -0300

    feat: add router delete and get chats

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from fastapi import FastAPI
 from src.shared.infrastructure.database import init_db
 from src.auth.api.routes.login import login_router as login_router
 from src.auth.api.routes.register import register_route as register_router
-from src.chatbot.api.routes import chat_routes as websocket_chat
+from chatbot.api.routes.chat import chat_routes as websocket_chat_router
+from chatbot.api.routes.delete_chat import delete_chat_route as delete_chat_router
+from chatbot.api.routes.get_chats import get_chats_route as get_chats_router
 from dotenv import load_dotenv
 
 
@@ -16,7 +18,9 @@ app = FastAPI()
 
 app.include_router(login_router.router, prefix="/auth")
 app.include_router(register_router.router, prefix="/auth")
-app.include_router(websocket_chat.router, prefix="/chat")
+app.include_router(websocket_chat_router.router, prefix="/chat")
+app.include_router(delete_chat_router.router, prefix="/chat")
+app.include_router(get_chats_router.router, prefix="/chat")
 
 async def main():
     config = uvicorn.Config(app, host="localhost", port=8000, log_level="info")
