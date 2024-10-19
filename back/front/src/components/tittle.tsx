31a8f56bb96dafd3e2963dcb160440171827bb06
Wellington Chida <wellingtonchida@protonmail.com>
2024-10-19 09:05:31 -0300

    remove this things

--- back/front/src/components/tittle.tsx
+++
@@ -1,9 +0,0 @@
-type TextProps = {
-    text: string;
-}
-
-export default function Tittle({text} : TextProps){
-    return(
-        <h1 className="text-2xl font-medium leading-[36px] font-poppins text-[#333333]">{text}</h1>
-    )
-}
--- back/mychat/src/shared/utils/get_services.py
+++
@@ -1,5 +0,0 @@
-
-
-class Utils:
-    def get_header(websocket, header_name):
-        return next((v for k, v in websocket.headers.items() if k.lower() == header_name), None)
