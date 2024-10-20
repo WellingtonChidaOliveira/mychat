f4d4c94956c6916a422f0fed04aeaaae01abea4c
Kauã Gomes <70298794+Kauagpaiva@users.noreply.github.com>
2024-10-20 19:59:58 -0300

    privando rota

--- front/src/app/(chat)/page.tsx
+++ front/src/app/(chat)/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation"; // ou 'next/router' dependendo da versão
 import Chat from "../../components/chat";
 
 interface PageProps {
@@ -5,6 +9,23 @@ interface PageProps {
 }
 
 export default function Home({ currentChatId }: PageProps) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true); // Estado de carregamento
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/auth");
+    } else {
+      setLoading(false); // Define o carregamento como falso se o token existir
+    }
+  }, [router]);
+
+  // Enquanto o estado de carregamento estiver verdadeiro, não renderize o conteúdo
+  if (loading) {
+    return <div>Loading...</div>; // Você pode substituir isso por um carregador mais estilizado
+  }
+
   return (
     <div className="bg-zinc-800 rounded-[10px] flex justify-center items-center w-[70%] h-full">
       <Chat currentChatId={currentChatId} />
